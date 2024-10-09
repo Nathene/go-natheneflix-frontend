fb7f43b87f2f9dbaeeeb8f46bc649a9e4c436044
Nathene <nathanvisnjic@gmail.com>
2024-10-09 16:52:21 +1100

    wire-framed front end

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './components/ErrorPage';
 import Home from './components/Home';
 import Movies from './components/Movies';
+import Movie from './components/Movie';
 import Genres from './components/Genres';
 import EditMovie from './components/EditMovie';
 import ManageCatalogue from './components/ManageCatalogue';
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
     children: [
       {index: true, element: <Home />},
       {path: "/movies", element: <Movies />},
+      {path: "/movies/:id", element: <Movie />},
       {path: "/genres", element: <Genres />},
       {path: "/admin/movie/0", element: <EditMovie />},
       {path: "/admin", element: <ManageCatalogue />},
